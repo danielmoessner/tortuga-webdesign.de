aee9272cb147cf43df50600898d7c31f37bec0f3
dmoetech <github@moesibert.de>
2020-10-05 19:31:06 +0200

    nuxt 404

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,90 +1,83 @@
 export default {
-  mode: 'universal',
-  target: 'static',
+  mode: "universal",
+  target: "static",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || 'Tortuga Webdesign',
+    title: process.env.npm_package_name || "Tortuga Webdesign",
     htmlAttrs: {
-      lang: 'de',
+      lang: "de"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-   ** Generate 
+   ** Generate
    */
-  // generate: {
-  //   routes: [
-  //     '/blog/ressourcen',
-  //     '/blog/webdesign-prozess',
-  //     '/portfolio/franco-pioli-datenschutz-und-vereinsmanagement',
-  //     '/portfolio/healed-me-i-won-the-lottery',
-  //     '/portfolio/internationale-gesellschaft-orientalischer-christen-ev',
-  //     '/portfolio/katzenbetreuung-tigerfreund',
-  //     '/portfolio/kristina-boess-yoga-und-coaching',
-  //     '/portfolio/maschinenverleih-schaetz',
-  //     '/portfolio/praeventionszentrum-muenchen-ost',
-  //     '/portfolio/qc-quality-control',
-  //     '/portfolio/rabe-sport-gmbh',
-  //     '/portfolio/schaetz-bautrockner-verleih',
-  //     '/portfolio/schaetz-shop-autozubehoer-fuer-ihren-mercedes',
-  //     '/portfolio/sefa-sahin-kues-gutachter',
-  //     '/portfolio/selma-selcuk-haarsysteme',
-  //     '/portfolio/silke-tolkmitt-marketingmanagement',
-  //     '/portfolio/sn-trockenbau-gmbh',
-  //     '/portfolio/the-corner-house',
-  //   ]
-  // },
+  generate: {
+    fallback: "404.html"
+    //   routes: [
+    //     '/blog/ressourcen',
+    //     '/blog/webdesign-prozess',
+    //     '/portfolio/franco-pioli-datenschutz-und-vereinsmanagement',
+    //     '/portfolio/healed-me-i-won-the-lottery',
+    //     '/portfolio/internationale-gesellschaft-orientalischer-christen-ev',
+    //     '/portfolio/katzenbetreuung-tigerfreund',
+    //     '/portfolio/kristina-boess-yoga-und-coaching',
+    //     '/portfolio/maschinenverleih-schaetz',
+    //     '/portfolio/praeventionszentrum-muenchen-ost',
+    //     '/portfolio/qc-quality-control',
+    //     '/portfolio/rabe-sport-gmbh',
+    //     '/portfolio/schaetz-bautrockner-verleih',
+    //     '/portfolio/schaetz-shop-autozubehoer-fuer-ihren-mercedes',
+    //     '/portfolio/sefa-sahin-kues-gutachter',
+    //     '/portfolio/selma-selcuk-haarsysteme',
+    //     '/portfolio/silke-tolkmitt-marketingmanagement',
+    //     '/portfolio/sn-trockenbau-gmbh',
+    //     '/portfolio/the-corner-house',
+    //   ]
+  },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
-  css: [
-    { src: '~/assets/styles/global-styles.scss', lang: 'scss' }
-  ],
+  css: [{ src: "~/assets/styles/global-styles.scss", lang: "scss" }],
 
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [
-    { src: "~/plugins/lazysizes.client.js" }
-  ],
+  plugins: [{ src: "~/plugins/lazysizes.client.js" }],
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: [
-    '@nuxtjs/gtm',
-  ],
+  buildModules: ["@nuxtjs/gtm"],
   /*
    ** Nuxt.js modules
    */
   modules: [
-    '@nuxtjs/markdownit',
-    'nuxt-purgecss',
-    '@nuxtjs/sitemap',
-    '@nuxtjs/svg',
+    "@nuxtjs/markdownit",
+    "nuxt-purgecss",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/svg"
   ],
   /*
-  ** Google Tag Manager Config
-  */
+   ** Google Tag Manager Config
+   */
   gtm: {
-    id: 'GTM-TQM6M58'
+    id: "GTM-TQM6M58"
   },
   /*
    ** Sitemap Config
    */
   sitemap: {
-    hostname: 'https://tortuga-webdesign.de',
+    hostname: "https://tortuga-webdesign.de"
   },
   /*
    ** Purgecss Config
@@ -96,9 +89,9 @@ export default {
    */
   markdownit: {
     injected: true,
-    preset: 'default',
+    preset: "default",
     linkify: true,
-    breaks: true,
+    breaks: true
   },
   /*
    ** Build configuration
@@ -110,9 +103,9 @@ export default {
           layouts: true,
           cacheGroups: {
             styles: {
-              name: 'styles',
+              name: "styles",
               test: /\.(css|vue)$/,
-              chunks: 'all',
+              chunks: "all",
               enforce: true
             }
           }
@@ -129,7 +122,8 @@ export default {
     },
     loaders: {
       scss: {
-        prependData: '@import "./assets/styles/insert-into-every-component.scss";',
+        prependData:
+          '@import "./assets/styles/insert-into-every-component.scss";'
       }
     },
     html: {
@@ -150,9 +144,9 @@ export default {
      */
     extend(config, { isDev, isClient, loaders: { vue } }) {
       if (isClient) {
-        vue.transformAssetUrls.img = ['data-src', 'src']
-        vue.transformAssetUrls.source = ['data-srcset', 'srcset']
+        vue.transformAssetUrls.img = ["data-src", "src"];
+        vue.transformAssetUrls.source = ["data-srcset", "srcset"];
       }
     }
   }
-}
+};
