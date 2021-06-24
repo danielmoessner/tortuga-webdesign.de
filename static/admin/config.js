b1eed3d42cb46c0b1895f4290c353c2154b7d211
danielmoessner <github@moesibert.de>
2021-06-24 17:03:04 +0200

    updates

--- static/admin/config.js
+++ static/admin/config.js
@@ -8,9 +8,14 @@ import packages from "../cms/packages.js";
 export default {
   backend: {
     name: "git-gateway",
+    branch: "main",
+    repo: "danielmoessner/tortuga-webdesign.de",
   },
   local_backend: true,
+  display_url: "https://tortuga-webdesign.de",
+  site_url: "https://tortuga-webdesign.de",
   media_folder: "/static/media",
   public_folder: "",
+  load_config_file: false,
   collections: [articles, tags, references, packages, services, pages],
 };
