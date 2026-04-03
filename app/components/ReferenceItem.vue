26c417f1fd2c8aa2ce0736eb9d0198649e5bbcd1
danielmoessner <github@danielmoessner.de>
2026-04-03 23:22:13 +0200

    fix: image size

--- app/components/ReferenceItem.vue
+++ app/components/ReferenceItem.vue
@@ -41,7 +41,6 @@
                 :src="portfolioItem.image"
                 :alt="portfolioItem.title"
                 width="420"
-                sizes="(min-width: 1024px) 33vw, 50vw"
                 format="avif,webp"
                 :preload="false"
                 :img-attrs="{
